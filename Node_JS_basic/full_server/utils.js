992b8022a059def429e244c9483608b3ee66d993
elcanaIiyev <elcanaliyevinfo@gmail.com>
2026-08-07 15:27:46 +0400

    Create utils.js

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1);
+
+    const fields = {};
+
+    students.forEach((line) => {
+      const parts = line.split(',');
+      const firstname = parts[0];
+      const field = parts[parts.length - 1];
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    });
+
+    resolve(fields);
+  });
+});
+
+module.exports = readDatabase;
